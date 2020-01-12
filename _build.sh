e5851c7c07665d3e5c0ba4e4d11c90f6fd7317f4
Dumitru Frunza <dumitru.frunza@gmail.com>
2020-01-11 20:23:04 -0500

    Delete and commit bin/*

--- _build.sh
+++ _build.sh
@@ -2,6 +2,7 @@ SRC_DIR=$PWD
 C_FLAGS="-std=gnu99 -ggdb -Winline -Wno-write-strings"
 L_FLAGS=""
 
+mkdir -p ./bin
 pushd >/dev/null ./bin
 gcc $C_FLAGS -I. -o drawing $SRC_DIR/drawing.c $L_FLAGS -lxcb -lxcb-icccm -lxcb-image -lm
 popd >/dev/null
