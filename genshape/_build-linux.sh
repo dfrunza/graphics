cbc2b844cd04cf7ead6423d841c732b044f0bff8
Dumitru Frunza <dumitru.frunza@noviflow.com>
2020-02-26 11:32:01 -0500

    Adjustment of genshape/_build-linux.sh

--- genshape/_build-linux.sh
+++ genshape/_build-linux.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-SRC_DIR=$PWD
-BIN_DIR=.
 C_FLAGS="-g -ggdb -std=gnu99 -Winline -Wno-write-strings -fms-extensions"
 L_FLAGS=""
 
-gcc $C_FLAGS -I. -I./include -o genshape $SRC_DIR/genshape.c -L. $L_FLAGS -l:./freetype-linux.a
+gcc $C_FLAGS -I. -Iinclude genshape.c -L. $L_FLAGS -l:freetype-linux.a -o genshape 
