fed55cf0ee79aaa6b9fd3ba5d391f36e30350705
Dumitru Frunza <dumitru.frunza@noviflow.com>
2020-02-27 12:55:07 -0500

    WIP

--- genshape/_build-linux.sh
+++ genshape/_build-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-C_FLAGS="-g -ggdb -std=gnu99 -Winline -Wno-write-strings -fms-extensions"
+C_FLAGS="-g -ggdb -std=gnu99 -Winline -Wno-write-strings -Wno-format-security -fms-extensions"
 L_FLAGS=""
 
 gcc $C_FLAGS -I. -Iinclude genshape.c -L. $L_FLAGS -l:freetype-linux.a -o genshape 
--- genshape/genshape.c
+++ genshape/genshape.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <wchar.h>
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
@@ -11,6 +12,15 @@ int main()
   char* font_name = "Px437_IBM_ISO8";
   int font_size_px = 16;
   int dpi = 72;
+  wchar_t char_list[] = L"ABC";
+
+  FILE* c_source_file = fopen("shape_data.c", "w");
+  if (!c_source_file) {
+    printf("ERROR\n");
+    return 1;
+  }
+  fwprintf(c_source_file, L"// -*- coding: utf-8 -*-\n\n");
+  fwprintf(c_source_file, L"#define FONT_NAME L\"%s\"\n\n", font_name);
 
   FT_Library  library;
   if (FT_Init_FreeType(&library)) {
@@ -28,8 +38,19 @@ int main()
     printf("ERROR\n");
     return 1;
   }
-  if (FT_Load_Glyph(face, 'A', FT_LOAD_NO_HINTING|FT_LOAD_NO_BITMAP)) {
-    printf("ERROR\n");
+  printf("%s.units_per_EM=%d\n", font_name, face->units_per_EM);
+
+  char* error = 0;
+  for (wchar_t* at = char_list; at != 0; ++at) {
+    wchar_t ch = *at;
+    if (FT_Load_Glyph(face, ch, FT_LOAD_NO_HINTING|FT_LOAD_NO_BITMAP)) {
+      error = "ERROR\n";
+      break;
+    }
+    FT_Outline outline = face->glyph->outline;
+  }
+  if (error) {
+    printf(error);
     return 1;
   }
   return 0;
