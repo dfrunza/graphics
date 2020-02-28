72b810c3d0ffb0588efad8a0806acb628d147a03
dfrunza <dumitru.frunza@gmail.com>
2020-02-27 23:00:55 -0500

    Add genshape/_build-haiku.sh file.

---
+++ genshape/_build-haiku.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+C_FLAGS="-g -ggdb -std=gnu99 -Winline -Wno-write-strings -Wno-format-security -fms-extensions"
+L_FLAGS=""
+
+gcc $C_FLAGS -I. -Iinclude genshape.c -L. $L_FLAGS -l:freetype-haiku.a -o genshape 
+
