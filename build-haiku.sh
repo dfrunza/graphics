4ffa06e036353213efd29c245f8427fcf5fa38b5
Dumitru Frunza <dumitru.frunza@gmail.com>
2020-03-09 23:02:27 -0400

    Remove the Haiku platform.

--- build-haiku.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-C_FLAGS="-g -ggdb -std=gnu++98 -Winline -Wno-write-strings -fms-extensions"
-L_FLAGS=""
-
-g++ $C_FLAGS -I. -o drawing haiku_main.cpp $L_FLAGS -lbe
-
--- genshape/build-haiku.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-C_FLAGS="-g -ggdb -std=gnu99 -Winline -Wno-write-strings -Wno-format-security -fms-extensions"
-L_FLAGS=""
-
-gcc $C_FLAGS -I. -Iinclude genshape.c -L. $L_FLAGS -l:freetype-haiku.a -o genshape 
-
